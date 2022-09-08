f0b77700988ffefe99c288b1b969af03f7ff16db
aman <amannegi229@gmail.com>
2022-09-08 09:58:51 +0530

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Aman Negi on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
